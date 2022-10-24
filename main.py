b2e89b97f08ade407cdd2ef21f4f54654a63b11a
leandro-lottermann <110182048+Leandro-Lottermann@users.noreply.github.com>
2022-10-24 09:18:19 -0300

    Testes finais
    A proxima tarefa será reescrever tudo para uma melhor organização

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from tkinter import *
 
-def cadastroTecnico():
-    janela = Tk()
-    janela.minsize(500,350)
+def clicou(label):
+    label["text"] = "clicou"
 
+def menuinicial():
+    janela = Tk()
+    janela.minsize(550,400)
+    janela.maxsize(550, 400)
+    janela.title("FERRAMENTARIA")
+
+    titulo = Label(janela, text="Escolha uma opção:", font="Arial 16")
+    titulo.place(relx=0.02, rely=0.05)
     imagem = PhotoImage(file="ferramentas.png")
 
-    button_ferramenta = Button(janela, image=imagem)
+    button_ferramenta = Button(janela, image=imagem, command=lambda: clicou(titulo))
 
-    button_ferramenta.pack(pady=20)
+    button_ferramenta.pack()
 
     janela.mainloop()
-cadastroTecnico()
+
+
+menuinicial()
