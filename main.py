eab8d8b54e66a727869974c53e6cabcd2082c242
leandro-lottermann <110182048+Leandro-Lottermann@users.noreply.github.com>
2022-10-24 10:36:00 -0300

    Finalização do layout inicial do menu principal

--- main.py
+++ main.py
@@ -1,23 +1,48 @@
 from tkinter import *
 
-def clicou(label):
-    label["text"] = "clicou"
+def cmd_ferramentas():
+    jan_ferramentaria = Tk()
+    jan_ferramentaria.minsize(550, 400)
+    jan_ferramentaria.maxsize(550, 400)
+    texto = Label(jan_ferramentaria, text="FERRAMENTAS", font="Arial 20")
+    texto.pack()
+    jan_ferramentaria.mainloop()
+def cmd_tecnicos():
+    jan_tecnicos = Tk()
+    jan_tecnicos.minsize(550, 400)
+    jan_tecnicos.maxsize(550, 400)
+    texto = Label(jan_tecnicos, text="TECNICOS", font="Arial 20")
+    texto.pack()
+    jan_tecnicos.mainloop()
 
 def menuinicial():
-    janela = Tk()
-    janela.minsize(550,400)
-    janela.maxsize(550, 400)
-    janela.title("FERRAMENTARIA")
+    m_window = Tk()
+    m_window.minsize(550, 400)
+    m_window.maxsize(550, 400)
+    m_window.title("FERRAMENTARIA - Menu Principal   v.1")
 
-    titulo = Label(janela, text="Escolha uma opção:", font="Arial 16")
-    titulo.place(relx=0.02, rely=0.05)
-    imagem = PhotoImage(file="ferramentas.png")
+    #TEXTOS:
+    text_titulo = Label(m_window, text="FERRAMENTARIA", font="Arial 18", borderwidth=2, relief="solid", pady=5, padx=5)
+    text_titulo.grid(row=1, column=1, columnspan=2, padx=8, pady=5)
+    text_selecione = Label(m_window, text="Escolha sua Opção:", font="Arial 14")
+    text_selecione.grid(row=2, column=0, pady=40,rowspan=2)
+    text_tecnicos = Label(m_window, text="Técnicos")
+    text_tecnicos.grid(row=6, column=1)
+    text_ferramentas = Label(m_window, text="Ferramentas")
+    text_ferramentas.grid(row=6,column=2)
 
-    button_ferramenta = Button(janela, image=imagem, command=lambda: clicou(titulo))
+    #botões:
+    img_fer = PhotoImage(file="ferramentas.png")
+    img_tec = PhotoImage(file= "tecnico.png")
 
-    button_ferramenta.pack()
+    b_ferramentas = Button(m_window, image=img_fer, command=cmd_ferramentas)
+    b_tecnicos = Button(m_window, image=img_tec, command=cmd_tecnicos)
+    b_tecnicos.grid(row=5,column=1)
+    b_ferramentas.grid(row=5, column=2)
 
-    janela.mainloop()
 
+
+
+    m_window.mainloop()
 
 menuinicial()
